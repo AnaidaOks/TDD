14958806053837e3af1b31c58ab12afde0b88957
Anaida Oksuzyan <aoa2190@gmail.com>
2024-07-15 18:56:51 +0300

    Stub and tests for add.

--- src/main/java/myThread/PhoneBook.java
+++ src/main/java/myThread/PhoneBook.java
@@ -2,4 +2,7 @@ package myThread;
 
 public class PhoneBook {
 
+    public int add(String name, String phone) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,13 @@
-public class PhoneBookTest {
+import myThread.PhoneBook;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PhoneBookTest {
+    @Test
+    public void testAdd(){
+        PhoneBook phoneBook = new PhoneBook();
+        int addedCount = phoneBook.add("Test", "999");
+        assertTrue(1 == addedCount, "Неверное добавление в телефонную книжку!");
+    }
 }
